e4707db2c94d34161378335a3f22547b558a42b9
Mikhail Poda <mikhail.poda@gmail.com>
2021-11-03 21:22:22 -0400

    Created folders for better structure

--- src/Workflow/Transformations.cs
+++ src/Workflow/Transformations.cs
@@ -2,21 +2,21 @@
 
 public static class Transformations
 {
+    private static string _pandoc = @"C:\Program Files\Pandoc\pandoc.exe";
+    private static string _srcPath = @"D:\code\phd-private";
+    
     public static void Doc2Md()
     {
-        var pandoc = @"C:\Program Files\Pandoc\pandoc.exe";
-        var srcPath = @"D:\code\phd-private";
-
-        var docxPath = Path.Combine(srcPath, "docx");
+        var docxPath = Path.Combine(_srcPath, "docx");
         var files = Directory.EnumerateFiles(docxPath, "*.docx");
 
         foreach (var file in files)
         {
             var fileName = Path.GetFileNameWithoutExtension(file);
-            var outFile = Path.Combine(srcPath, "md", fileName + ".md");
+            var outFile = Path.Combine(_srcPath, "md", fileName + ".md");
             var args = new[] {"--from=docx", "--to=markdown", "--output=" + outFile, file};
 
-            System.Diagnostics.Process.Start(pandoc, args);
+            System.Diagnostics.Process.Start(_pandoc, args);
 
             Thread.Sleep(2000);
         }
@@ -24,17 +24,15 @@ public static class Transformations
     
     public static void Md2Tex()
     {
-        var pandoc = @"C:\Program Files\Pandoc\pandoc.exe";
-        var srcPath = @"D:\code\phd-private";
-        var mdPath = Path.Combine(srcPath, "md");
+        var mdPath = Path.Combine(_srcPath, "md");
         var files = Directory.EnumerateFiles(mdPath, "*.md").ToList();
 
         foreach (var file in files)
         {
             var fileName = Path.GetFileNameWithoutExtension(file);
-            var outFile = Path.Combine(srcPath, "tex", fileName + ".tex");
+            var outFile = Path.Combine(_srcPath, "tex", fileName + ".tex");
             var args = new[] {"--from=markdown-auto_identifiers", "--to=latex", "--output=" + outFile, file};
-            var result = System.Diagnostics.Process.Start(pandoc, args);
+            var result = System.Diagnostics.Process.Start(_pandoc, args);
 
             Console.WriteLine(result);
         }
@@ -42,14 +40,12 @@ public static class Transformations
     
     public static void Md2Docx()
     {
-        var pandoc = @"C:\Program Files\Pandoc\pandoc.exe";
-        var srcPath = @"D:\code\phd-private\md";
-
-        var file = Path.Combine(srcPath, "document.md");
-        var outFile = Path.Combine(srcPath, "document.docx");
+        var mdPath = Path.Combine(_srcPath, "md");
+        var file = Path.Combine(mdPath, "document.md");
+        var outFile = Path.Combine(mdPath, "document.docx");
 
         var args = new[] {"--from=markdown", "--to=docx", "--output=" + outFile, file};
-        var result = System.Diagnostics.Process.Start(pandoc, args);
+        var result = System.Diagnostics.Process.Start(_pandoc, args);
 
         Console.WriteLine(result.ExitCode);
     }
